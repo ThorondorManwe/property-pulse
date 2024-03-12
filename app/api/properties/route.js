fa50ec79769e5ca35695f7062bf3f987f3125ca7
Carlos Rangel <charlesleimaginant@gmail.com>
2024-03-12 17:20:40 -0600

    Database connection with Mongoose and my first API route

---
+++ app/api/properties/route.js
@@ -0,0 +1,11 @@
+import connectDB from "../../../config/database.js";
+
+export const GET = async (request) => {
+    try {
+        await connectDB();
+        return new Response(JSON.stringify({message: 'Hello World!'}), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new Response('Something went Wrong', { status: 500 });
+    }
+};
