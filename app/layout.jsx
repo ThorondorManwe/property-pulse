27aebdee2d00302b69c45bb7e3b7dc07bb86ee09
Carlos Rangel <charlesleimaginant@gmail.com>
2024-03-21 14:51:45 -0600

    Save User to the Database & Session

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "../assets/styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AuthProvider from "../components/AuthProvider";
+import "../assets/styles/globals.css";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
