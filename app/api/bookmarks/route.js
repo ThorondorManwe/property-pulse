336ca87148c1067d2e08b6249a0e6f32e55d1152
Carlos Rangel <charlesleimaginant@gmail.com>
2024-03-31 17:16:20 -0600

    Saved Properties

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -4,6 +4,30 @@ import Property from "../../../models/Property";
 import { getSessionUser } from "../../../utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
+// GET /api/bookmarks
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const { userId } = await getSessionUser();
+
+    // Find the user by their session ID
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return new Response("User not found", { status: 404 });
+    }
+
+    // Retrieve the user's bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
 
 export const POST = async (request) => {
   try {
