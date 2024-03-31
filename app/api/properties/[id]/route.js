0837ebfaa969c7f3ad517ca655ba0fcf2cd29503
Carlos Rangel <charlesleimaginant@gmail.com>
2024-03-31 00:40:05 -0600

    Edit Property Form and Update Put API Route

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -54,3 +54,65 @@ export const DELETE = async (request, { params }) => {
     return new Response("Something went wrong", { status: 500 });
   }
 };
+
+// PUT /api/properties/:id
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = params;
+    const { userId } = await getSessionUser();
+    const formData = await request.formData();
+
+    // Access all values for amenities
+    const amenities = formData.getAll("amenities");
+
+    // Fetch the existing property's data
+    const existingProperty = await Property.findById(id);
+
+    if (!existingProperty) {
+      return new Response("Property Not Found", { status: 404 });
+    }
+
+    // Check if the user is the owner of the property
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // Create the propertyData object with updated values
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly."),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+
+    // Update the property with the propertyData object
+    const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+    return new Response(JSON.stringify(updatedProperty), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response("Failed to edit property", { status: 500 });
+  }
+};
