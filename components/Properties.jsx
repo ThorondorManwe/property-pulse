b41b1456f85c9f0fb73667165e5dc4797dccdbd5
Carlos Rangel <charlesleimaginant@gmail.com>
2024-04-17 15:29:34 -0600

    Pagination Component

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -2,35 +2,40 @@
 import { useState, useEffect } from "react";
 import PropertyCard from "./PropertyCard";
 import Spinner from "./Spinner";
+import Pagination from "./Pagination";
+
 const Properties = () => {
     const [properties, setProperties] = useState([]);
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(3);
+    const [pageSize, setPageSize] = useState(6);
     const [totalItems, setTotalItems] = useState(0);
 
     useEffect(() => {
         const fetchProperties = async () => {
+        try {
+            const res = await fetch(
+            `/api/properties?page=${page}&pageSize=${pageSize}`
+            );
 
-            try {
-                const res = await fetch(
-                `/api/properties?page=${page}&pageSize=${pageSize}`
-                );
-        
-                if (!res.ok) {
-                throw new Error('Failed to fetch data');
-                }
-        
-                const data = await res.json();
-                setProperties(data.properties);
-                setTotalItems(data.total);
-                setLoading(false);
-            } catch (error) {
-                console.error(error);
+            if (!res.ok) {
+            throw new Error("Failed to fetch data");
             }
-            };
+
+            const data = await res.json();
+            setProperties(data.properties);
+            setTotalItems(data.total);
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+        }
+        };
         fetchProperties();
     }, [page, pageSize]);
+
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+    };
 
     return loading ? (
         <Spinner />
@@ -47,6 +52,8 @@ const Properties = () => {
                 ))}
             </div>
             )}
+
+            <Pagination page={page} pageSize={pageSize} totalItems={totalItems} onPageChange={handlePageChange} />
         </div>
         </section>
     );
